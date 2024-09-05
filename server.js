c6baa949e198174fab5f874113cfc9fe570319ae
kwakjaehun <jh5414092@icloud.com>
2024-09-05 17:00:13 +0900

    Add an access root

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Hello World");
+});
+
 app.listen(8080, (req, res) => {
   res.json({ message: "Hello World" });
 });
