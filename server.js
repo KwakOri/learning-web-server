982b56d9be85d3103e7734caeb138b3261baec95
kwakjaehun <jh5414092@icloud.com>
2024-09-05 17:23:30 +0900

    send로 수정

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json("Hello World");
+  res.send("Hello World");
 });
 
 app.listen(3000, (req, res) => {
